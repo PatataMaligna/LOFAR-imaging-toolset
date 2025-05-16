3a18c340ab2258e25fbff4357a1b41734e673e16
Jorge <jorgecuello217@gmail.com>
2025-05-16 22:45:22 +0300

    git hub set up fv

--- setup.sh
+++ setup.sh
@@ -4,15 +4,12 @@
 set -e
 
 echo "Creating virtual environment..."
-python3 -m venv envLofar
+python3.12 -m venv envLofar
 source envLofar/bin/activate
 
 echo "Installing Python dependencies..."
 pip install --upgrade pip
-pip install git+https://github.com/lofar-astron/lofarimaging.git
-cd realtime_processor
 pip install -r requirements.txt
-cd ..
 
 echo "To start the app, run:"
 echo "source envLofar/bin/activate && python3 -m realtime_processor.main /path/to/your/data"
