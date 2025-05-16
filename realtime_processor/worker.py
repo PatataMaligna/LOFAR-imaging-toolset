7ac37dd14bd6673a2c7e4bb6884752f33aa3eef0
Jorge <jorgecuello217@gmail.com>
2025-05-16 10:23:52 +0300

    testing time

--- realtime_processor/worker.py
+++ realtime_processor/worker.py
@@ -102,7 +102,7 @@ class DataProcessorWorker(QObject):
 
                         while self.waiting_for_plot:
                             QCoreApplication.processEvents()                            
-                            time.sleep(1)
+                            # time.sleep(1)
 
                         pbar.update(last_size - prev_size)
                         start_time = time.time()
